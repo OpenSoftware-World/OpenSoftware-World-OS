81d70ee79f275457bc451d8de7322bfb2ffdffd2
OpenSoftware-World <143949134+OpenSoftware-World@users.noreply.github.com>
2026-02-15 21:35:23 +0300

    Minor edit.

--- programs/reboot.asm
+++ programs/reboot.asm
@@ -6,27 +6,19 @@ section .text
 global reboot
 
 reboot:
-    cli                 ; Interruptları kapat
+    cli                 
 
-    ; --------------------------
-    ; Method 1: BIOS Bootstrap
-    ; --------------------------
     xor ax, ax
-    int 0x19            ; BIOS bootstrap loader
-
-    ; Eğer BIOS reset etmezse buraya düşer
-    ; --------------------------
-    ; Method 2: Keyboard Controller Reset
-    ; --------------------------
+    int 0x19            
 
 .wait_kbc:
-    in al, 0x64         ; Keyboard controller status
-    test al, 0x02       ; Input buffer dolu mu?
-    jnz .wait_kbc       ; Doluysa bekle
+    in al, 0x64         
+    test al, 0x02       
+    jnz .wait_kbc       
 
-    mov al, 0xFE        ; CPU reset komutu
+    mov al, 0xFE        
     out 0x64, al
 
 .hang:
-    hlt                 ; Reset olmazsa burada kal
+    hlt                 
     jmp .hang
--- programs/shutdown.asm
+++ programs/shutdown.asm
@@ -1,24 +1,19 @@
 org 100h
 
 start:
-
-    ; APM var mı kontrol et
     mov ax, 5300h
     int 15h
     jc no_apm
 
-    ; Real mode bağlantı
     mov ax, 5301h
     int 15h
     jc no_apm
 
-    ; Power management enable
     mov ax, 5308h
     mov bx, 0001h
     mov cx, 0001h
     int 15h
 
-    ; Sistemi kapat
     mov ax, 5307h
     mov bx, 0001h
     mov cx, 0003h
