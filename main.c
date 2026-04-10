002f2f8c4260b20b3b84a49e29c37b29e4a6cf03
OpenSoftware-World <143949134+OpenSoftware-World@users.noreply.github.com>
2026-04-11 01:15:14 +0300

    The main.c file, which is the most important file for OpenSoftware-World OS, has been split into two parts along with the system_init.c file.

--- main.c
+++ main.c
@@ -4,31 +4,11 @@
 #include "OpenKernel/SystemLib/TimeMng/time.h"
 #include "OSServices/Shell/shell.h"
 
-void kernel_main()
+void os_main(void)
 {
-    // The system is starting up and displaying log messages
-    vga_init();
-    sleep(30);
-    log_message("S", "System disk read successful; loading the kernel...");
-    sleep(30);
-    log_message("I", "Kernel initialized successfully.");
-    sleep(20);
-    log_message("I", "VGA Driver initalized successfully.");
-    sleep(10);
-    log_message("I", "RTC Services initalized successfully.");
-    sleep(10);
-    log_message("I", "Shell initalized successfully.");
-    sleep(5);
-    log_message("W", "Since this system is still new, there may be some glitches.");
-    sleep(15);
-    log_message("U", "The hardware status is unknown.");
-    sleep(25);
-    log_message("S", "The system is ready to start; the system is starting.");
-    sleep(100);
-
     // Main menu
     vga_set_color_scheme(VGA_COLOR_BLUE, VGA_COLOR_WHITE);
-    vga_screen_clear();
+    vga_clear_screen("F");
     vga_print_scr_nw("Welcome to the OpenSoftware-World OS!");
     vga_print_scr_nw("OpenSoftware-World OS version 1.0");
 
---
+++ system_init.c
@@ -0,0 +1,30 @@
+#include "OpenKernel/Drivers/Vga/vga.h"
+#include "OpenKernel/SystemLib/SysCalls/basic_syscall.h"
+#include "OpenKernel/SystemLib/KernelFunc/kernel_func.h"
+#include "OpenKernel/SystemLib/TimeMng/time.h"
+
+void system_init()
+{
+    // The system is starting up and displaying log messages
+    vga_init();
+    sleep(30);
+    log_message("S", "System disk read successful; loading the kernel...");
+    sleep(30);
+    log_message("I", "Kernel initialized successfully.");
+    sleep(20);
+    log_message("I", "VGA Driver initalized successfully.");
+    sleep(10);
+    log_message("I", "RTC Services initalized successfully.");
+    sleep(10);
+    log_message("I", "Shell initalized successfully.");
+    sleep(5);
+    log_message("W", "Since this system is still new, there may be some glitches.");
+    sleep(15);
+    log_message("U", "The hardware status is unknown.");
+    sleep(25);
+    log_message("S", "The system is ready to start; the system is starting.");
+    sleep(100);
+
+    // Jump to the main menu
+    exec_func("os_main");
+}
